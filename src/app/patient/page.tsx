1ef96d9dc1cba29d4e60ffb055fa414c1e214f30
JehanPinto <judepinto73@gmail.com>
2025-02-26 22:33:32 +0530

    Update page.tsx

--- src/app/patient/page.tsx
+++ src/app/patient/page.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 import MultiActionAreaCard from "./MultiActionAreaCard"
 import AboutUs from "../components/aboutUs"
 
+
+import "./style.css";
+
 import WhyChooseUs from "../components/whyChooseUs"
 const page = () => {
   return (
     <div>
       {/* <WhyChooseUs/> */}
-      <MultiActionAreaCard />
+      {/* <MultiActionAreaCard /> */}
       {/* <AboutUs/> */}
+      
     </div>
   )
 }
