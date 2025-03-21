6d5975ecb23743fd1eefd440e4482a52527e3dce
Sithum001 <128293308+Sithum001@users.noreply.github.com>
2025-03-21 14:22:41 +0530

    commit of sithum

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -26,6 +26,8 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        //className={`${process.env.NODE_ENV === "development" ? "debug-screens" : "" }  ${inter.className} antialiased `}
+        
       >
         {children}
       </body>
