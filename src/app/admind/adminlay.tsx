a36dfdc31a98b3cc266054bf19fa63b4d3df44af
gimhanijayamanna <pabasarajayamanna@gmail.com>
2025-03-21 14:23:21 +0530

    admin layout

---
+++ src/app/admind/adminlay.tsx
@@ -0,0 +1,11 @@
+import AdminDashboard from "./AdminDashboard";
+
+const AdminPage = () => {
+    return (
+        <div className="bg-[#E6EFF5] min-h-screen">
+            <AdminDashboard />
+        </div>
+    );
+};
+
+export default AdminPage;
