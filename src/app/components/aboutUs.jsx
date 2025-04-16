d5306c9f76898b3cc1cbac216da91c710339861d
JehanPinto <judepinto73@gmail.com>
2025-04-16 11:29:48 +0530

    about us updated

--- src/app/components/aboutUs.jsx
+++ src/app/components/aboutUs.jsx
@@ -2,54 +2,38 @@ import React from 'react'
 
 const AboutUs = () => {
   return (
-    <div>
-      <section
-        id="features"
-       className="relative block px-6 py-10 md:py-20 md:px-10 border-t border-b border-neutral-900 bg-neutral-900/30 my-20 mx-auto max-w-7xl z-10 grid grid-cols-1 gap-4 pt-14 sm:grid-cols-2 lg:grid-cols-2"
-       >
-        <div className="relative mx-auto max-w-2xl text-center bg-slate-900">
-          <h1 className="bg-gradient-to-b from-white to-gray-400 bg-clip-text text-transparent  my-3 flex items-center justify-center text-3xl sm:text-4xl md:text-5xl font-bold uppercase tracking-wider">
-            About Us
-          </h1>
+    <div className="relative min-h-screen bg-[#E1F3FD] px-6">
+            <div className="absolute left-0 w-3/5 flex items-center justify-start h-full px-6 pl-20">
 
-         
-          
+                <div className="md:w-full text-left space-y-8">
+                    <h1 className="text-[80px] font-black text-[#1045A1] leading-tight break-words" style={{ fontFamily: 'Poppins' }}>
+                        Healing is your right!<br />Claim it now!
+                    </h1>
+                    <p className="text-gray-700 text-[24px] leading-relaxed">
+                        Your journey to healing, peace, and happiness starts 
+                        right here, at <span className="font-semibold">MindfulConnect</span>.  
+                        At this safe place, we connect you with licensed therapists ready 
+                        to help you find your inner peace.
+                    </p>
+                    <Link href="/">
+                        <button className="mt-9 bg-[#70C6E6] text-[17px] text-gray-900 font-medium px-6 py-3 rounded-full flex items-center gap-2 hover:bg-[#8EC2EB] transition">
+                            Take the first step today →
+                        </button>
+                    </Link>
+                </div>
+            </div>
+
+            <div className="absolute right-0 md:w-3/7 flex justify-end mt-12 md:mt-0 m-0 p-0" style={{ right: '-2%' }}>
+                <Image 
+                    width={678}
+                    height={1036} 
+                    src="/HeroImg.png"
+                    alt="Smiling woman representing healing"
+                    priority 
+                    className="rounded-lg drop-shadow-lg"
+                />
+            </div>
         </div>
-        <div className="relative mx-auto max-w-2xl text-center bg-slate-900">
-          <h1 className="bg-gradient-to-b from-white to-gray-400 bg-clip-text text-transparent  my-3 flex items-center justify-center text-3xl sm:text-4xl md:text-5xl font-bold uppercase tracking-wider">
-            About Us
-          </h1>
-
-         
-          
-        </div>
-
-        
-        
-      </section>
-    </div>
-  )
-}
-const FeatureItem = ({ icon, title, description }) => {
-  return (
-    <div className="rounded-2xl bg-[#16354a] p-8 text-center shadow transition duration-300 hover:bg-[#1f4865]">
-      <div
-        className="button-text mx-auto flex h-12 w-12 items-center justify-center rounded-md border transition duration-300 hover:scale-110"
-        style={{
-          backgroundImage:
-            "linear-gradient(rgb(206, 206, 219) 0%, rgb(10, 73, 121) 100%)",
-          borderColor: "rgb(87, 87, 94)",
-        }}
-      >
-        {icon}
-      </div>
-      <h3 className="mt-6 bg-gradient-to-b from-white to-gray-400 bg-clip-text text-transparent \">
-        {title}
-      </h3>
-      <p className="my-4 mb-0 font-normal leading-relaxed tracking-wide bg-gradient-to-b from-white to-gray-400 bg-clip-text text-transparent ">
-        {description}
-      </p>
-    </div>
   );
 };
 
