2555c1edebf7c35482a7b8f63308768e3be42ca5
Sithum001 <128293308+Sithum001@users.noreply.github.com>
2025-05-02 14:54:01 +0530

    faq
    faq

--- src/app/components/Faq.tsx
+++ src/app/components/Faq.tsx
@@ -9,8 +9,8 @@ const Faq = () => {
   return (
     <div className="min-h-screen relative p-4 pb-10 ">
       <section>
-        <h1 className="flex gap-3 iteam-center" >
-            <span className="font-bold  text-5xl font-sans text-blue-600 pb-10 ">Frequently Asked Questions</span>
+        <h1 className="flex   gap-3 iteam-center" >
+            <span className="font-bold  text-5xl font-sans text-blue-600 pb-10  ">Frequently Asked Questions</span>
         </h1>
       </section>
       <section className="relative bg-white transition-all mx-auto max-w-xs sm:max-w-2xl rounded-lg flex-col gap-4 mt-40 sm:mt-8 p-10
